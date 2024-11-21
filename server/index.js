12af175fb7edad9b0addefad0df21ff3aa16136a
MoayadAlismail <moayadalismail@gmail.com>
2024-11-20 21:52:53 -0500

    block of code from the girl

--- server/index.js
+++ server/index.js
@@ -3,8 +3,27 @@ const { createServer } = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 
-const app = express();
+// const app = express();
+const express = require("express");
 
+const app = express();
+const port = process.env.SERVER_PORT || 8000;
+
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/api", (req, res) => {
+  res.json("Hello");
+});
+
+app.listen(port, () => console.log(Listening on port ${port}));
 app.use(cors({
   origin: ['https://www.balibalik.com', 'http://localhost:3000'],
   methods: ['GET', 'POST'],

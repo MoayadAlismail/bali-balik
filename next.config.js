bbde39254b30b86e32558015bf147322b4a73a77
MoayadAlismail <moayadalismail@gmail.com>
2024-11-21 19:30:40 -0500

    fixed the "invalid field: destination for route"

--- next.config.js
+++ next.config.js
@@ -4,13 +4,20 @@ const nextConfig = {
     return [
       {
         source: '/:path*',
-        destination: 'http://bali-balik-production.up.railway.app',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://bali-balik-production.up.railway.app' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
           { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
         ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/socket.io/:path*',
+        destination: 'https://bali-balik-production.up.railway.app/socket.io/:path*',
       },
     ];
   },

ae6b41acff952ff8a7af16014692c9033185bb89
MoayadAlismail <moayadalismail@gmail.com>
2024-11-30 22:42:43 -0500

    server config

--- server/index.js
+++ server/index.js
@@ -42,7 +42,8 @@ const io = new Server(httpServer, {
   pingTimeout: 60000,
   pingInterval: 25000,
   secure: true,
-  rejectUnauthorized: true
+  rejectUnauthorized: false,
+  maxHttpBufferSize: 1e8,
 });
 
 // Socket connection handling

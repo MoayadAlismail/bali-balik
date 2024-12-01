67a94e6a389f853a356cde479f1771807c3803b2
MoayadAlismail <moayadalismail@gmail.com>
2024-12-01 18:51:02 -0500

    degrading to http

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config = {
   serverUrl: process.env.NODE_ENV === 'production' 
-    ? 'https://balibalik.up.railway.app'
+    ? 'http://balibalik.up.railway.app'
     : 'http://localhost:3000',
   wsPath: '/socket.io/'
 };

3f754c41dd554822e375b7740347a3297717ff9e
Muathmua <moath.animations@Gmail.com>
2024-12-02 00:08:00 -0500

    changed port to what is listed on railway

--- server/index.js
+++ server/index.js
@@ -426,7 +426,7 @@ app.use((req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 httpServer.listen(PORT, '0.0.0.0', () => {
   console.log('==================================');
   console.log(`Server running on port ${PORT}`);

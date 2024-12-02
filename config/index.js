b2988c1dbc7ee60c9070367951e832c3299a028e
MoayadAlismail <moayadalismail@gmail.com>
2024-12-01 19:08:20 -0500

    upgraded protocol

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config = {
   serverUrl: process.env.NODE_ENV === 'production' 
-    ? 'http://balibalik.up.railway.app'
+    ? 'https://balibalik.up.railway.app'
     : 'http://localhost:3000',
   wsPath: '/socket.io/'
 };

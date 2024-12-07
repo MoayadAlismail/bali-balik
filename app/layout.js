1d49d61ca5b494e8899b2876420b6a7d31c5e8d0
MoayadAlismail <moayadalismail@gmail.com>
2024-12-07 17:28:23 -0500

    .en subdomain for translation

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
+      <head>
+        <link rel="alternate" hreflang="ar" href="https://www.balibalik.com" />
+        <link rel="alternate" hreflang="en" href="https://en.balibalik.com" />
+        <script 
+          type="text/javascript" 
+          src="https://cdn.weglot.com/weglot.min.js"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              Weglot.initialize({
+                api_key: 'wg_ac65c208f7ceaf68132a894c73b20e960'
+              });
+            `
+          }}
+        />
+      </head>
       <body className={`${handjet.className}`}>
         <main className="min-h-screen">
           {children}
